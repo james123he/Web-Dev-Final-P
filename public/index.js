2a647384484e6e167ee99a8e7beb61dbf74a04f1
Michael Heredos <michael.heredos@gmail.com>
2024-12-11 07:23:01 -0800

    some modifications to styles and gave the upload button an id, as well as starting working on client side js

---
+++ public/index.js
@@ -0,0 +1,18 @@
+console.log("== document:", document)
+console.log("== document.body:", document.body)
+
+var uploadBackdrop = document.getElementById("upload-backdrop")
+console.log("== upload-backdrop:", uploadBackdrop)
+
+var uploadBox = document.getElementById("upload-box")
+console.log("== upload-box:", uploadBox)
+
+var uploadButton = document.getElementById("upload-button")
+console.log("== upload-button:", uploadButton)
+
+uploadButton.addEventListener("click", function() {
+	uploadBackdrop.classList.remove("hide-upload")
+	uploadBackdrop.classList.add("show-upload")
+	uploadBox.classList.add("show-upload")
+})
+
