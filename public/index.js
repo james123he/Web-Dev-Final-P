e39f0b379093f8fd0896ec7c2e2b783332917fc1
Michael Heredos <michael.heredos@gmail.com>
2024-12-11 07:49:05 -0800

    made upload button bring up upload box

--- public/index.js
+++ public/index.js
@@ -13,6 +13,7 @@ console.log("== upload-button:", uploadButton)
 uploadButton.addEventListener("click", function() {
 	uploadBackdrop.classList.remove("hide-upload")
 	uploadBackdrop.classList.add("show-upload")
+	uploadBackdrop.classList.remove("hide-upload")
 	uploadBox.classList.add("show-upload")
 })
 
